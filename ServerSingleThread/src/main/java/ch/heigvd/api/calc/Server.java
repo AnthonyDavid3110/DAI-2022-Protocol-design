58807da2c50b1978e5b74dd4d60eda7a640a308e
SNSantos <dedale.sigma@gmail.com>
2022-11-03 12:00:18 +0100

    Not functional

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -26,12 +26,23 @@ public class Server {
     /**
      * Start the server on a listening socket.
      */
-    private void start() {
+    private void start() throws IOException {
         /* TODO: implement the receptionist server here.
          *  The receptionist just creates a server socket and accepts new client connections.
          *  For a new client connection, the actual work is done by the handleClient method below.
          */
-
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        try {
+            serverSocket = new ServerSocket(2019);
+            clientSocket = serverSocket.accept();
+            handleClient(clientSocket);
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, ex.getMessage());
+        } finally {
+        clientSocket.close();
+        serverSocket.close();
+        }
     }
 
     /**
@@ -40,6 +51,18 @@ public class Server {
      * @param clientSocket with the connection with the individual client.
      */
     private void handleClient(Socket clientSocket) {
+        LOG.log(Level.INFO, "A client has arrived.");
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+        try {
+            reader  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+            writer  = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(),"UTF-8"));
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
 
         /* TODO: implement the handling of a client connection according to the specification.
          *   The server has to do the following:
@@ -50,6 +73,7 @@ public class Server {
          *     - Handle the message
          *     - Send to result to the client
          */
+        ;
 
     }
 }

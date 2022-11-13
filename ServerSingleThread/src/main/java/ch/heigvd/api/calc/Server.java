d8ddc9114e8179fdfe5bb490909081dcc5daa805
SNSantos <dedale.sigma@gmail.com>
2022-11-13 19:25:19 +0100

    Update Server.java

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -10,6 +10,16 @@ import java.util.logging.Logger;
  * Calculator server implementation - single threaded
  */
 public class Server {
+    private int port;
+
+    public Server(int port){
+        this.port = port;
+    }
+    public Server(){
+        this(2019);
+    }
+
+
 
     private final static Logger LOG = Logger.getLogger(Server.class.getName());
 
@@ -26,7 +36,7 @@ public class Server {
     /**
      * Start the server on a listening socket.
      */
-    private void start() throws IOException {
+    private void start() {
         /* TODO: implement the receptionist server here.
          *  The receptionist just creates a server socket and accepts new client connections.
          *  For a new client connection, the actual work is done by the handleClient method below.
@@ -34,14 +44,29 @@ public class Server {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         try {
-            serverSocket = new ServerSocket(2019);
-            clientSocket = serverSocket.accept();
-            handleClient(clientSocket);
+            serverSocket = new ServerSocket(port);
         } catch (IOException ex) {
-            LOG.log(Level.SEVERE, ex.getMessage());
-        } finally {
-        clientSocket.close();
-        serverSocket.close();
+            LOG.log(Level.SEVERE, null, ex);
+            return;
+        }
+
+        while(true){
+            LOG.log(Level.INFO, "Single-threaded: Waiting for a new client on port {0}", port);
+            try {
+                clientSocket = serverSocket.accept();
+                handleClient(clientSocket);
+                clientSocket.close();
+
+            } catch (IOException ex) {
+                if (clientSocket != null) {
+                    try {
+                        clientSocket.close();
+                    } catch (IOException ex1) {
+                        LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
+                    }
+                }
+                LOG.log(Level.SEVERE, ex.getMessage(), ex);
+            }
         }
     }
 
@@ -51,18 +76,6 @@ public class Server {
      * @param clientSocket with the connection with the individual client.
      */
     private void handleClient(Socket clientSocket) {
-        LOG.log(Level.INFO, "A client has arrived.");
-        BufferedReader reader = null;
-        BufferedWriter writer = null;
-        try {
-            reader  = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
-            writer  = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(),"UTF-8"));
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
 
         /* TODO: implement the handling of a client connection according to the specification.
          *   The server has to do the following:
@@ -73,7 +86,36 @@ public class Server {
          *     - Handle the message
          *     - Send to result to the client
          */
-        ;
+        BufferedReader in = null;
+        BufferedWriter out = null;
 
+        while(true){
+            try {
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
+                String line;
+
+                out.write("Welcome to the Single-Threaded Server.\nSend me text lines and conclude with the BYE command.\n");
+                out.flush();
+
+
+            } catch (IOException ex) {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException ex1) {
+                        LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
+                    }
+                }
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException ex1) {
+                        LOG.log(Level.SEVERE, ex1.getMessage(), ex1);
+                    }
+                }
+                LOG.log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
     }
 }
